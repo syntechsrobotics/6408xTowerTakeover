29e6eb95b953b02e496286035f5d4f5ebd2b19bf
priydev007 <45751039+priydev007@users.noreply.github.com>
2019-12-16 22:21:23 -0800

    auto testing
    made some changes to the auto script

--- SensorTesting/src/main.cpp
+++ SensorTesting/src/main.cpp
@@ -10,8 +10,8 @@
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
-// RangeFinderE         sonar         E, F
-// RangeFinderG         sonar         G, H
+// RangeFinderE         sonar         E, F            
+// RangeFinderG         sonar         G, H            
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -20,14 +20,14 @@ using namespace vex;
 
 // A global instance of competition
 competition Competition;
-vex::motor FrontLeft_Motor = vex::motor(vex::PORT4, false);
-vex::motor FrontRight_Motor = vex::motor(vex::PORT3, true);
-vex::motor BackLeft_Motor = vex::motor(vex::PORT2, false);
-vex::motor BackRight_Motor = vex::motor(vex::PORT1, true);
+vex::motor FrontLeft_Motor = vex::motor(vex::PORT13, false);
+vex::motor FrontRight_Motor = vex::motor(vex::PORT17, true);
+vex::motor BackLeft_Motor = vex::motor(vex::PORT6, false);
+vex::motor BackRight_Motor = vex::motor(vex::PORT18, true);
 vex::controller Controller = vex::controller();
 vex::motor Ramp_Motor = vex::motor(vex::PORT14, true);
 vex::motor Arm_Motor = vex::motor(vex::PORT19, true);
-vex::motor Left_Intake_Motor = vex::motor(vex::PORT11, true);
+vex::motor Left_Intake_Motor = vex::motor(vex::PORT12, true);
 vex::motor Right_Intake_Motor = vex::motor(vex::PORT20, false);
 vex::vision Vision = vex::vision(vex::PORT15);
 
@@ -41,14 +41,16 @@ const int VISION_HEIGHT = 210;
 const int VISION_MAX_DISTANCE = 30000;
 const int VISION_OFFSET = 0;
 
+bool button_switch = true;
+
 const int PURPLE = 0;
 const int GREEN = 1;
 int level_count = 0;
 int DR4B_LEVEL = 0;
 
-
-
-// void LimitSwitchPressed() { Arm_Motor.stop(); }
+float speed_L = 0;
+float speed_R = 0;
+//void LimitSwitchPressed() { Arm_Motor.stop(); }
 
 // define your global instances of motors and other devices here
 // Absolute value function, i.e: x=|x|
@@ -76,11 +78,11 @@ double capMinMax(double val, double min, double max) {
   return val;
 }
 int Ramp_power() {
-  float Ramp_Speed;
-  Ramp_Speed = 100 - Ramp_Motor.rotation(rotationUnits::deg) / 12;
+  float Ramp_Speed = 100 - Ramp_Motor.rotation(rotationUnits::deg)/14;
+  
   /*if (Ramp_Motor.rotation(rotationUnits::deg) <= 400) {
     Ramp_Speed = 100;
-  } else if (400 < Ramp_Motor.rotation(rotationUnits::deg) <= 450) {
+  } else if (400 < Ramp_Motor.rotation(rotationUnits::deg) <= 500) {
     Ramp_Speed = 50;
   } else {
     Ramp_Speed = 10;
@@ -107,10 +109,14 @@ void driveForLeftRight(float leftRevs, float rightRevs, int speed) {
                             velocityUnits::pct, true);
 }
 void swaysSpin(float leftSpeed, int rightSpeed = 0) {
-  FrontLeft_Motor.spin(directionType::fwd, leftSpeed, velocityUnits::pct);
-  BackLeft_Motor.spin(directionType::rev, leftSpeed, velocityUnits::pct);
-  FrontRight_Motor.spin(directionType::rev, rightSpeed, velocityUnits::pct);
-  BackRight_Motor.spin(directionType::fwd, rightSpeed, velocityUnits::pct);
+  FrontLeft_Motor.spin(directionType::fwd, leftSpeed,
+                            velocityUnits::pct);
+  BackLeft_Motor.spin(directionType::rev, leftSpeed,
+                           velocityUnits::pct);
+  FrontRight_Motor.spin(directionType::rev, rightSpeed,
+                             velocityUnits::pct);
+  BackRight_Motor.spin(directionType::fwd, rightSpeed,
+                            velocityUnits::pct);
 }
 void driveFor(float inches, int speed = 85) {
   FrontLeft_Motor.rotateFor(inToRev(inches), rotationUnits::rev, speed,
@@ -138,10 +144,9 @@ void intakeoff(void) {
 }
 void initiate(void) {
 
-  Left_Intake_Motor.spin(directionType::fwd, 100, velocityUnits::pct);
-  Right_Intake_Motor.spin(directionType::fwd, 100, velocityUnits::pct);
-  driveFor(5, 10);
-  intakeoff();
+  
+  driveFor(5, 100);
+  
 }
 void scoretower(void) {
   Ramp_Motor.rotateFor(-1.2, rotationUnits::rev, 100, velocityUnits::pct);
@@ -157,98 +162,128 @@ void Intake(bool dir, int speed) {
     Left_Intake_Motor.spin(directionType::fwd, speed, velocityUnits::pct);
     Right_Intake_Motor.spin(directionType::fwd, speed, velocityUnits::pct);
   } else {
-    Left_Intake_Motor.spin(directionType::fwd, -90, velocityUnits::pct);
-    Right_Intake_Motor.spin(directionType::fwd, -90, velocityUnits::pct);
+    Left_Intake_Motor.spin(directionType::rev, speed, velocityUnits::pct);
+    Right_Intake_Motor.spin(directionType::rev, speed, velocityUnits::pct);
   }
 }
-int Raise_DR4B( ) {
 
- int level=2;
-   int raiseSpeed = 100;
+int Raise_DR4B() {
   float degRamp = Ramp_Motor.rotation(rotationUnits::deg);
   float degDR4B = Arm_Motor.rotation(rotationUnits::deg);
-  if (level == 1) {
+  if (level_count == 1) {
     degRamp = 500;
     degDR4B = -650;
-  } else if (level == 2) {
+  } else if (level_count == 2) {
     degRamp = 600;
     degDR4B = -850;
-  } else if (level == 3) {
+  } else if (level_count == 3) {
     degRamp = 800;
     degDR4B = -1300;
-  } else if (level == 0) {
+  } else if (level_count == 0) {
     degRamp = 0;
     degDR4B = 0;
-    raiseSpeed = 55;
+    
   }
-  Ramp_Motor.rotateTo(degRamp, rotationUnits::deg, raiseSpeed,
-                      velocityUnits::pct, false);
-  // task::sleep(100);
+
+  
+  
   Arm_Motor.rotateTo(degDR4B, rotationUnits::deg, 100, velocityUnits::pct,
-                     true);
+                     false);
+
+  if(Ramp_Motor.rotation(rotationUnits::deg) >= degRamp){
+    vex::this_thread::sleep_for( 300 );
+  }
+  Ramp_Motor.rotateTo(degRamp, rotationUnits::deg, 100,
+                      velocityUnits::pct, true);
+  button_switch = true;
+
   return 0;
 }
+
 void stack(void) {
-  Ramp_Motor.rotateFor(2.27, rotationUnits::rev, 100, velocityUnits::pct);
-  Intake(1, 60);
-  task::sleep(500);
-  driveFor(-10, 20);
+  //Ramp_Motor.rotateTo(600, rotationUnits::deg, 100, velocityUnits::pct,true);
+
+  //Ramp_Motor.rotateTo(800, rotationUnits::deg, 60, velocityUnits::pct,true);
+ // Ramp_Motor.rotateTo(1000, rotationUnits::deg, 20, velocityUnits::pct,true);
+  Left_Intake_Motor.stop(brakeType::coast);
+  Right_Intake_Motor.stop(brakeType::coast);
+  while(Ramp_Motor.rotation(rotationUnits::deg)<1020){
+  Ramp_Motor.spin(directionType::fwd, Ramp_power(), velocityUnits::pct);
+  }
+  Ramp_Motor.stop(brakeType::hold);
+
+  Intake(1, 90);
+  task::sleep(100);
+  driveFor(-10, 10);
   intakeoff();
 }
 
-void scoretower(bool height) {}
-#pragma region "Sonar rotation code"
-void SonarRotation(float maxPower = 55, float minPower = 3) {
+
+//void scoretower(bool height) {}
+
+
+#pragma region "Sonar code"
+//Stops when reaches teh correct distance
+
+//for Rotation
+int SonarRotation() {
+  float maxPower = 55;
+  float minPower = 3;
   float kp = 2;
-  /*float sonar1val = ;
-  float sonar2val = RangeFinderG.distance(inches);*/
   float error = 0;
   int ticksElapsed = 0;
-  int timeTicks = 0;
-  bool atTarget = false;
-  //float targetReading = 0;
+  int ticksTime = 0;
+  int Max_Time = 50;//is not in millisecond, it is how many loops it runs.
+  // Real time(millisecond) should be "Max_Time*(sleep time in loop)".
   float drivePower = 0;
 
-  while (!atTarget) {
-    error = RangeFinderE.distance(inches) - RangeFinderG.distance(inches);
+  while (ticksElapsed < 3 && ticksTime < Max_Time) {
+    error = RangeFinderG.distance(inches) - RangeFinderE.distance(inches);
   
 
     drivePower = error * kp;
     drivePower = capMinMax(drivePower, minPower, maxPower);
-    setLeftRightDriveSpeed(-drivePower, drivePower);
-    if (abs(error) <1 ) {
-      //ticksElapsed = 0;
-      atTarget = true;
-    
-    }
+    //setLeftRightDriveSpeed(-drivePower, drivePower);
+    speed_L = -drivePower;
+    speed_R = drivePower;
+
+    Controller.Screen.clearScreen();
     Controller.Screen.setCursor(1, 1);
     Controller.Screen.print(error);
-    //Controller.Screen.print(drivePower);
-    //Controller.Screen.clearLine();
-    //Controller.Screen.newLine();
     Controller.Screen.setCursor(2, 1);
     Controller.Screen.print(RangeFinderE.distance(inches));
-    //Controller.Screen.newLine();
     Controller.Screen.setCursor(3, 1);
     Controller.Screen.print(RangeFinderG.distance(inches));
-    task::sleep(20);
-    Controller.Screen.clearScreen();
-/*
-    if (ticksElapsed > 4) {
-      atTarget = true;
-       
-    }*/
-   
-   // ticksElapsed++;
-    //timeTicks++;
+
+
+    if(ticksTime >= Max_Time){
+      Controller.Screen.clearScreen();
+      Controller.Screen.setCursor(1, 1);
+      Controller.Screen.print("Time out !!!");
+    }
+    vex::this_thread::sleep_for( 20 );
+    
+
+    if (abs(drivePower) < 1){
+      ticksElapsed++;
+    }else{
+      ticksElapsed = 0;
+    }
+
+    ticksTime++;
+    //ticksElapsed++;
   }
-  setLeftRightDriveSpeed(0, 0);
+  speed_L = 0;
+  speed_R = 0;
+  
+  //setLeftRightDriveSpeed(0, 0);
+  return 0;
 }
 #pragma endregion
 
 #pragma region "vision code"
 
-int getVisionObjX(int colour) {
+int getVisionObjX() {
   // if (colour == PURPLE)
   Vision.takeSnapshot(SIG_1);
   // else
@@ -285,26 +320,25 @@ int getVisionObjX(int colour) {
 }
 
 // This is the camera code.
-void aimRobot(int colour) {
+int aimRobot() {
   // initialize P loop variables
   float maxPower = 50;
   float minPower = 2;
-  int timeOut = 4000 / 67; // 4000 / 67;
-  float kp = .10;          // this is the kp. Change this down to make
+  float kp = .10;         // this is the kp. Change this down to make
 
   // Init timer stuff.
   int ticksElapsed = 0;
-  int timeTicks = 0;
   float error = 0;
   float drivePower = 0;
   // finish check variables
-  bool atTarget = false;
+  int ticksTime = 0;
+  int Max_Time = 100;
 
   // run motors until target is within n px certainty
-  while (!atTarget || (timeTicks < timeOut)) {
+  while (ticksElapsed < 3 && ticksTime < Max_Time) {
     // Find distance from center.
 
-    int disFromCenter = getVisionObjX(colour);
+    int disFromCenter = getVisionObjX();
     error = disFromCenter - VISION_OFFSET;
 
     // This means robot didn't find a target; exits
@@ -320,39 +354,46 @@ void aimRobot(int colour) {
     setLeftRightDriveSpeed(drivePower, -drivePower);
 
     // check for finish
-    if (abs(error) > 9) {
+    if (abs(error) < 3){
+      ticksElapsed++;
+    }else{
       ticksElapsed = 0;
     }
-    if (ticksElapsed > 3) {
-      atTarget = true;
+
+    ticksTime++;
+
+    if(ticksTime >= Max_Time){
+      Controller.Screen.clearScreen();
+      Controller.Screen.setCursor(1, 1);
+      Controller.Screen.print("Time out !!!");
     }
-    ticksElapsed++;
-    timeTicks++;
-    task::sleep(HZ_15_WAIT);
+    vex::this_thread::sleep_for( HZ_15_WAIT );
+
   }
   setLeftRightDriveSpeed(0, 0);
+  return 0;
 }
 
-void SwaysRobot(int colour) {
-  // initialize P loop variables
+
+int SwaysRobot() {
+// initialize P loop variables
   float maxPower = 50;
-  float minPower = 4;
-  int timeOut = 59; // 4000 / 67;
-  float kp = .3;    // this is the kp. Change this down to make
+  float minPower = 2;
+  float kp = .4;         // this is the kp. Change this down to make
 
   // Init timer stuff.
   int ticksElapsed = 0;
-  int timeTicks = 0;
   float error = 0;
   float drivePower = 0;
   // finish check variables
-  bool atTarget = false;
+  int ticksTime = 0;
+  int Max_Time = 3;
 
   // run motors until target is within n px certainty
-  while (!atTarget || (timeTicks < timeOut)) {
+  while (ticksElapsed < 3 && ticksTime < Max_Time) {
     // Find distance from center.
 
-    int disFromCenter = getVisionObjX(colour);
+    int disFromCenter = getVisionObjX();
     error = disFromCenter - VISION_OFFSET;
 
     // This means robot didn't find a target; exits
@@ -368,20 +409,28 @@ void SwaysRobot(int colour) {
     swaysSpin(drivePower, drivePower);
 
     // check for finish
-    if (abs(error) > 9) {
+    if (abs(error) < 3){
+      ticksElapsed++;
+    }else{
       ticksElapsed = 0;
     }
-    if (ticksElapsed > 3) {
-      atTarget = true;
+
+    ticksTime++;
+
+    if(ticksTime >= Max_Time){
+      Controller.Screen.clearScreen();
+      Controller.Screen.setCursor(1, 1);
+      Controller.Screen.print("Time out !!!");
     }
-    ticksElapsed++;
-    timeTicks++;
-    task::sleep(HZ_15_WAIT);
+    vex::this_thread::sleep_for( HZ_15_WAIT );
   }
   setLeftRightDriveSpeed(0, 0);
+  return 0;
 }
 
 #pragma endregion
+
+
 
 void pre_auton(void) {
   // Initializing Robot Configuration. DO NOT REMOVE!
@@ -391,6 +440,52 @@ void pre_auton(void) {
   // Example: clearing encoders, setting servo positions, ...
 }
 
+
+
+void sonar_sideways(int Time, float speed){
+int count = 0;
+  while(count<Time){
+    float greatest_speed = 0;
+
+    float sonar_speed = (RangeFinderG.distance(inches) - RangeFinderE.distance(inches))*20;
+
+
+    float motor_speed[] = {speed-sonar_speed,-speed-sonar_speed,-speed+sonar_speed,speed+sonar_speed};
+
+    for(int i = 0; i < 4; i++) {
+      if (motor_speed[i]>100){
+        greatest_speed = motor_speed[i];
+      }
+    }
+    if (greatest_speed>100){
+
+      float rate = 100/greatest_speed;
+      for(int i = 0; i < 4; i++) {
+        motor_speed[i] = motor_speed[i]*rate;
+      }
+    }
+
+
+    FrontLeft_Motor.spin(directionType::fwd, motor_speed[0], velocityUnits::pct);
+    BackLeft_Motor.spin(directionType::fwd, motor_speed[1], velocityUnits::pct);
+    FrontRight_Motor.spin(directionType::fwd, motor_speed[2], velocityUnits::pct);
+    BackRight_Motor.spin(directionType::fwd, motor_speed[3], velocityUnits::pct);
+
+    Controller.Screen.clearScreen();
+    Controller.Screen.setCursor(1, 1);
+    Controller.Screen.print(speed_L);
+    Controller.Screen.setCursor(2, 1);
+    Controller.Screen.print(speed_R);
+    task::sleep( 20 );
+    
+
+   count++;
+   }
+   FrontLeft_Motor.stop();
+    BackLeft_Motor.stop();
+    FrontRight_Motor.stop();
+    BackRight_Motor.stop();
+   }
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
 /*                              Autonomous Task                              */
@@ -400,31 +495,47 @@ void pre_auton(void) {
 /*                                                                           */
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
+void combine(){
+
+}
+
+
+
+
+
 
 void autonomous(void) {
-  /*initiate();
-
-  aimRobot(PURPLE);
-  driveFor(20, 70);
-  driveFor(-20, 40);
-  task::sleep(500); // goes for the first set of boxes
-
-  driveFor(-35, 70);
-  task::sleep(500);
-   // retreats*/
-
-  // driveFor(15, 49);
-  SwaysRobot(PURPLE);
-  /*Intake(0, 45);
-  driveFor(40, 40);
-  task::sleep(300);
+  //initiate();
+  Intake(0, 100); 
+  //SwaysRobot(PURPLE);
+  //aimRobot(PURPLE);
+  driveFor(33, 90);
+  driveFor(20, 50);
+  driveForLeftRight(-.3,.3, 100);
+  driveFor(-37, 100);
   intakeoff();
-  driveFor(-38, 40);
-
-  driveForLeftRight(1.7,-1.7, 40);
-  sideways(29, 70);*/
-  // driveFor(7, 70);
-  // stack();
+  driveFor(-30,100);
+  
+  //sideways(49, 100);
+   //thread myThread = thread(SwaysRobot);
+   //thread sonar_thread = thread(SonarRotation);
+   sonar_sideways(2,90);
+  //sideways(7, 100);
+  //aimRobot();
+  SwaysRobot();
+  
+  Intake(0, 100); 
+  driveFor(50, 60);
+  //task::sleep(300);
+  driveFor(-12, 100);
+  intakeoff();
+  driveFor(-40, 100);
+  driveForLeftRight(1.33,-1.33, 50);
+  driveFor(11, 40);
+  swaysSpin(30,30);
+  task::sleep(50);
+  stack();
+  
 }
 
 /*---------------------------------------------------------------------------*/
@@ -485,43 +596,57 @@ void usercontrol(void) {
 
     if (Controller.ButtonL2.pressing()) {
       Ramp_Motor.spin(directionType::fwd, Ramp_power(), velocityUnits::pct);
+      button_switch = true;
     } else if (Controller.ButtonL1.pressing()) {
       Ramp_Motor.spin(directionType::rev, 100, velocityUnits::pct);
-    } else {
+      button_switch = true;
+    } else if(button_switch == true){
       if (Ramp_Motor.rotation(rotationUnits::deg) >= 400) {
         Ramp_Motor.stop(brakeType::hold);
       } else {
         Ramp_Motor.stop(brakeType::coast);
       }
     }
-    if (Controller.ButtonRight.pressing()) {
-      SonarRotation();
-    }
-    if (Controller.ButtonA.pressing()) {
 
+  if (Controller.ButtonLeft.pressing()){
+stack();
+  }
+
+    if (Controller.ButtonRight.pressing()) {
+      aimRobot();
+      //SonarRotation();
+    }
+
+  
+    if (Controller.ButtonA.pressing()) {
+      if(button_switch == true){
+      button_switch = false;
       level_count += 1;
       if (level_count > 3) {
         level_count = 0;
       }
       thread myThread = thread(Raise_DR4B);
-    }
-    if (Controller.ButtonY.pressing()) {
-
+      }
+    }else if (Controller.ButtonY.pressing()) {
+      if(button_switch == true){
+      button_switch = false;
       level_count = 0;
       if (level_count < 0) {
         level_count = 3;
       }
-      ///Raise_DR4B(level_count);
-    
-      
+      thread myThread = thread(Raise_DR4B);
+      }
     }
+
 
     // ..........................................................................
     if (Controller.ButtonB.pressing()) {
       Arm_Motor.spin(directionType::fwd, 100, velocityUnits::pct); // up
+      button_switch = true;
     } else if (Controller.ButtonX.pressing()) {
       Arm_Motor.spin(directionType::rev, 100, velocityUnits::pct); // down
-    } else {
+      button_switch = true;
+    } else if(button_switch == true){
       Arm_Motor.stop(brakeType::hold);
     }
 
@@ -532,9 +657,14 @@ void usercontrol(void) {
     } else if (Controller.ButtonR1.pressing()) {
       Left_Intake_Motor.spin(directionType::rev, 100, velocityUnits::pct);
       Right_Intake_Motor.spin(directionType::rev, 100, velocityUnits::pct);
-    } else {
-      Left_Intake_Motor.stop(brakeType::hold);
-      Right_Intake_Motor.stop(brakeType::hold);
+    } else{
+      if (Ramp_Motor.rotation(rotationUnits::deg) <= 1000) {
+        Left_Intake_Motor.stop(brakeType::hold);
+        Right_Intake_Motor.stop(brakeType::hold);
+      } else {
+        Left_Intake_Motor.stop(brakeType::coast);
+        Right_Intake_Motor.stop(brakeType::coast);
+      }
     }
 
     if (Controller.ButtonUp.pressing()) {
@@ -548,15 +678,21 @@ void usercontrol(void) {
       FrontRight_Motor.spin(directionType::rev, 10, velocityUnits::pct);
       BackRight_Motor.spin(directionType::rev, 10, velocityUnits::pct);
     }
-    /*Controller.Screen.newLine();
-    Controller.Screen.setCursor(2, 1);
-    Controller.Screen.print(Ramp_Motor.rotation(rotationUnits::deg));
-    Controller.Screen.newLine();
-    Controller.Screen.setCursor(3, 1);
-    Controller.Screen.print(Ramp_power());*/
-    // Brain.Screen.print(RangeFinderG.distance(inches));
+
+    Controller.Screen.setCursor(1,1);
+    Controller.Screen.print(level_count);
+    Controller.Screen.setCursor(2,1);
+    Controller.Screen.print(Arm_Motor.rotation(rotationUnits::deg));
+
+
+
+    Brain.Screen.print(RangeFinderE.distance(inches));
+    //Brain.Screen.print(RangeFinderG.distance(inches));
     wait(20, msec); // Sleep the task for a short amount of time to
-    Controller.Screen.clearLine(); // prevent wasted resources.
+    Brain.Screen.clearLine(); // prevent wasted resources.
+
+
+    Controller.Screen.clearScreen();
   }
 }
 
@@ -575,4 +711,5 @@ int main() {
   while (true) {
     wait(100, msec);
   }
+
 }
---
+++ SensorTesting/src/robot-config.cpp
@@ -0,0 +1,25 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+sonar RangeFinderE = sonar(Brain.ThreeWirePort.E);
+sonar RangeFinderG = sonar(Brain.ThreeWirePort.G);
+
+// VEXcode generated functions
+
+
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
